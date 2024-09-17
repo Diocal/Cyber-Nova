ee24a75655bb2ee75f60445382e0070babac7b8e
diego calderon <diegocalderon9999@gmail.com>
2024-09-17 09:12:21 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,23 @@ import Header from '../components/Header';
 import AboutUs from '@/components/AboutUs';
 import Cybersecurity from '@/components/Cybersecurity';
 import ArtificialIntelligence from '../components/ArtificialIntelligence';
-import Services from '../components/Services';  // Asegúrate de que Services esté importado
-import AppDevelopment from '../components/AppDevelopment';  // Asegúrate de que AppDevelopment esté importado
+import Services from '../components/Services';
+import AppDevelopment from '../components/AppDevelopment';
 import OurMissionAndTeam from '../components/OurMissionAndTeam';
 import Footer from '../components/Footer';
-export default function Layout({ children }: { children: React.ReactNode }) {
+
+export default function Page() {  // Cambié 'Layout' por 'Page'
   return (
     <>
-
       <main>
-        {/* Aquí añades el componente Hero */}
         <Header />
-
-        {/* Mover el componente Services arriba de App Development */}
         <Services />
-
-        {/* Después agregas App Development */}
         <AppDevelopment />
         <Cybersecurity/>
         <ArtificialIntelligence/>
         <AboutUs/>
         <OurMissionAndTeam/>
         <Footer />
-        {children}
       </main>
     </>
   );
